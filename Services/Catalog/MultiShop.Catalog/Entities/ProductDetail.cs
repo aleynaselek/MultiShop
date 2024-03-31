d1201b1fd40253bc60d45aa3de2032e3a5c7f3c4
aleynaflo <selekaleyna@gmail.com>
2024-03-31 21:50:16 +0300

    8. ProductImage ve Detail Entityleri

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -2,5 +2,9 @@
 {
     public class ProductDetail
     {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+
     }
 }
