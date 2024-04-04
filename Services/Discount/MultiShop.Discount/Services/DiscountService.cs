f1a9d77e59f305ee405edb94203e3a054b3c4e02
aleynaflo <selekaleyna@gmail.com>
2024-04-04 07:50:56 +0300

    29-Yeni indirim kuponu tanımlama service

--- Services/Discount/MultiShop.Discount/Services/DiscountService.cs
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -1,12 +1,30 @@
-﻿using MultiShop.Discount.Dtos;
+﻿using Dapper;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Dtos;
 
 namespace MultiShop.Discount.Services
 {
     public class DiscountService : IDiscountService
     {
-        public Task CreateCouponAsync(CreateCouponDto createCouponDto)
+        private readonly DapperContext _context;
+
+        public DiscountService(DapperContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task CreateCouponAsync(CreateCouponDto createCouponDto)
+        {
+            string query = "insert into Coupons (Code,Rate,IsActive,ValidDate) values (@code,@rate,@isActive,@validDate)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@code", createCouponDto.Code);
+            parameters.Add("@rate", createCouponDto.Rate);
+            parameters.Add("@isActive", createCouponDto.IsActive);
+            parameters.Add("@validDate", createCouponDto.ValidDate);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            } 
         }
 
         public Task DeleteCouponAsync(int id)
