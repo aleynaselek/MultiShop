f3d2d1d365dca04c64de296cbbebfa63fc989896
aleynaflo <selekaleyna@gmail.com>
2024-04-16 00:51:53 +0300

    118-Redis Sepet Test İşlemi

--- Services/Basket/MultiShop.Basket/Dtos/BasketItemDto.cs
+++ Services/Basket/MultiShop.Basket/Dtos/BasketItemDto.cs
@@ -2,7 +2,7 @@
 {
     public class BasketItemDto
     {
-        public int ProductId { get; set; }
+        public string ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
