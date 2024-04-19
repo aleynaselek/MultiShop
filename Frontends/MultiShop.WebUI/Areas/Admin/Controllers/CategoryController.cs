dcc9ea27a9c648fb17de8d6e328cb5d7b4e4a941
aleynaflo <selekaleyna@gmail.com>
2024-04-20 01:49:50 +0300

    147-Kategori Sayfası Hazırlığı

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -4,8 +4,13 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
+        [Area("Admin")]
         public IActionResult Index()
         {
+            ViewBag.v1="Ana Sayfa";
+            ViewBag.v2="Kategoriler";
+            ViewBag.v3="Kategori Listesi";
+            ViewBag.v0 = "Kategori İşlemleri";
             return View();
         }
     }
