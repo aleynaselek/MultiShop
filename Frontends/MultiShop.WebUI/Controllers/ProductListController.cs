8833ee15ecd8484eca21036038ac84ca5191260d
aleynaflo <selekaleyna@gmail.com>
2024-05-13 00:11:28 +0300

    225-Yorumların Product Detail Sayfadına Taşınması

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index(string id)
         {
             ViewBag.i = id;
@@ -14,6 +23,28 @@ namespace MultiShop.WebUI.Controllers
         {
             ViewBag.x = id;
             return View();
+        } 
+        
+         
+        public async Task<PartialViewResult> AddComment(string id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7076/api/Comments/CommentListByProductId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return PartialView(value);
+            }
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(CreateCommentDto createCommentDto) 
+        {
+            return RedirectToAction("Index", "Default");
+                
         }
     }
 }
