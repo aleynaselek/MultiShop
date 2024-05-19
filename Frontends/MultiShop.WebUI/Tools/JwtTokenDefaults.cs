bdae144e574d68fd5b14ef89164ec0691e66bc4e
aleynaflo <selekaleyna@gmail.com>
2024-05-19 23:49:27 +0300

    243-JwtTokenDefault ve GetCheckAppUserViewModel sınıfları

---
+++ Frontends/MultiShop.WebUI/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.WebUI.Tools
+{
+	public class JwtTokenDefaults
+	{
+		public const string ValidAudience = "http://localhost";
+		public const string ValidIssuer = "http://localhost";
+		public const string Key = "MultiShop.....09876543123456.netcore8.0.*+/-";
+		public const int Expire = 60;
+    }
+}
