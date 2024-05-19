1d35370e904084e9ac8c7e32f5d6eae30f62a5ed
aleynaflo <selekaleyna@gmail.com>
2024-05-19 16:46:08 +0300

    234-Login Formu

---
+++ Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+using MultiShop.DtoLayer.IdentityDtos.RegisterDtos; 
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
+        {   
+            
+				var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(createLoginDto);
+                StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PostAsync("http://localhost:5001/api/Registers", stringContent);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            return View();
+
+        }
+    }
+}
