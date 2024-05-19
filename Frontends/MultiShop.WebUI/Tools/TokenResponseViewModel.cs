a14490211b88b491cd51eae0f1a081acc41382fd
aleynaflo <selekaleyna@gmail.com>
2024-05-20 00:03:26 +0300

    244-JwtTokenGenerator

---
+++ Frontends/MultiShop.WebUI/Tools/TokenResponseViewModel.cs
@@ -0,0 +1,14 @@
+namespace MultiShop.WebUI.Tools
+{
+	public class TokenResponseViewModel
+	{
+		public TokenResponseViewModel(string token, DateTime expireDate)
+		{
+			Token = token;
+			ExpireDate = expireDate;
+		}
+
+		public string Token { get; set; } 
+        public DateTime ExpireDate { get; set; } 
+    }
+}
