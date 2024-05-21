49c1207e55ae60bd890ab4964fab05050d4642a7
aleynaflo <selekaleyna@gmail.com>
2024-05-21 21:10:53 +0300

    248-Authorize Attribute

--- Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
+++ Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
@@ -5,9 +5,7 @@ namespace MultiShop.WebUI.Controllers
     public class DefaultController : Controller
     {
         public IActionResult Index()
-        {
-            var user = User.Claims;
-            int x;
+        { 
             return View();
         }
     }
