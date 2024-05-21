2c8a227f037530dfa7ac3ba3d82dd883b734df0f
aleynaflo <selekaleyna@gmail.com>
2024-05-21 21:31:48 +0300

    251-Test Controller

---
+++ Frontends/MultiShop.WebUI/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.DtoLayer.CatalogDtos.ContactDtos;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        } 
+
+        
+        public async Task<IActionResult> Index()
+        {              
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7072/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+
+        }
+    }
+}
