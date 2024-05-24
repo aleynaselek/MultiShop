d3c07583bdb886860cb522d8e8902a99c85fd72e
aleynaflo <selekaleyna@gmail.com>
2024-05-24 20:25:58 +0300

    287-UI'da Kullanıcı Verilerinin Listelenmesi

--- Frontends/MultiShop.WebUI/Handlers/ResourceOwnerPasswordTokenHandler.cs
+++ Frontends/MultiShop.WebUI/Handlers/ResourceOwnerPasswordTokenHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using MultiShop.WebUI.Services.Interfaces;
 using System.Net;
+using System.Net.Http.Headers;
 using static System.Net.WebRequestMethods;
 
 namespace MultiShop.WebUI.Handlers
@@ -22,14 +23,14 @@ namespace MultiShop.WebUI.Handlers
         {
 
             var accessToken = await _contextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
-            request.Headers.Authorization = new BasicAuthenticationHeaderValue("Bearer",accessToken);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",accessToken);
             var response = await base.SendAsync(request, cancellationToken);
             if(response.StatusCode == HttpStatusCode.Unauthorized) 
             {
                 var tokenResponse = await _identityService.GetRefreshToken();
                 if (tokenResponse != null)
                 {
-                    request.Headers.Authorization = new BasicAuthenticationHeaderValue("Bearer", accessToken);
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
                     response = await base.SendAsync(request, cancellationToken);
                 }
             }
