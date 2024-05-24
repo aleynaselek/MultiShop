2893ad5ddce1303f56e562653dc0168b73f91f68
aleynaflo <selekaleyna@gmail.com>
2024-05-24 18:57:50 +0300

    284-Identity User Controller

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+       
+		public UsersController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		[HttpGet]
+        public async Task<IActionResult> GetUserInfo()
+        {             
+            var userClaim = User.Claims.FirstOrDefault(x=>x.Type == JwtRegisteredClaimNames.Sub);
+            var user = await _userManager.FindByNameAsync(userClaim.Value);
+
+            return Ok(new
+            {
+				Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                Email = user.Email,
+                Usrename = user.UserName
+            } ); 
+        }
+    }
+}
