22b6ee87f827d345683a5f89e9927369ea896479
aleynaflo <selekaleyna@gmail.com>
2024-05-24 22:08:04 +0300

    293-ClientCredential Service Sınıfı

---
+++ Frontends/MultiShop.WebUI/Services/Concrete/ClientCredentialTokenService.cs
@@ -0,0 +1,53 @@
+﻿using IdentityModel.AspNetCore.AccessTokenManagement;
+using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Settings;
+
+namespace MultiShop.WebUI.Services.Concrete
+{
+    public class ClientCredentialTokenService : IClientCredentialTokenService
+    {
+        private readonly IClientAccessTokenCache _clientAccessTokenCache;
+        private readonly HttpClient _httpClient;
+        private readonly ClientSettings _clientSettings;
+        private readonly ServiceApiSettings _serviceApiSettings;
+
+        public ClientCredentialTokenService(IClientAccessTokenCache clientAccessTokenCache, HttpClient httpClient, IOptions<ClientSettings> clientSettings, IOptions<ServiceApiSettings> serviceApiSettings)
+        {
+            _clientAccessTokenCache = clientAccessTokenCache;
+            _httpClient = httpClient;
+            _clientSettings = clientSettings.Value;
+            _serviceApiSettings = serviceApiSettings.Value;
+        }
+
+        public async Task<string> GetToken()
+        {
+            var token1 = await _clientAccessTokenCache.GetAsync("multishoptoken");
+
+            if(token1 != null)
+            {
+                return token1.AccessToken;
+            }
+
+            var discoveryEndPoint = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceApiSettings.IdentityServerUrl,
+                Policy = new DiscoveryPolicy
+                {
+                    RequireHttps = false,
+                }
+            });
+            var clientCredentialTokenRequest = new ClientCredentialsTokenRequest
+            {
+                ClientId = _clientSettings.MultiShopVisitorClient.ClientId,
+                ClientSecret = _clientSettings.MultiShopVisitorClient.ClientSecret,
+                Address = discoveryEndPoint.TokenEndpoint
+            };
+
+            var token2 = await _httpClient.RequestClientCredentialsTokenAsync(clientCredentialTokenRequest);
+            await _clientAccessTokenCache.SetAsync("multishoptoken", token2.AccessToken, token2.ExpiresIn);
+            return token2.AccessToken;
+        }
+    }
+}
