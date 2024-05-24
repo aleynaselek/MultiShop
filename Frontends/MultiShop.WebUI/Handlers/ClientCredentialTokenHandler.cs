8569d5305e878606ffec15a2f80019c85ad4e6ff
aleynaflo <selekaleyna@gmail.com>
2024-05-24 20:32:35 +0300

    288-Client Credential Token Handler sınıfı

---
+++ Frontends/MultiShop.WebUI/Handlers/ClientCredentialTokenHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using MultiShop.WebUI.Services.Concrete;
+using MultiShop.WebUI.Services.Interfaces;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace MultiShop.WebUI.Handlers
+{
+    public class ClientCredentialTokenHandler : DelegatingHandler
+    {
+        private readonly IClientCredentialTokenService _clientCredentialTokenService;
+        private readonly IIdentityService _identityService;
+
+        public ClientCredentialTokenHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public ClientCredentialTokenHandler(IClientCredentialTokenService clientCredentialTokenService)
+        {
+            _clientCredentialTokenService = clientCredentialTokenService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+             
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",await _clientCredentialTokenService.GetToken());
+            var response = await base.SendAsync(request, cancellationToken);
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                var tokenResponse = await _identityService.GetRefreshToken();
+                if (tokenResponse != null)
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                    response = await base.SendAsync(request, cancellationToken);
+                }
+            }
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                //hata mesajı
+            }
+            return response;
+        }
+    }
+}
