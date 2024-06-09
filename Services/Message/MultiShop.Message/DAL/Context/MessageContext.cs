395533652fcb84646ac0753bc6be0dad02191f10
aleynaflo <selekaleyna@gmail.com>
2024-06-09 20:48:16 +0300

    379-PostgreSQL  Bağlantı Konfigürasyonu

--- Services/Message/MultiShop.Message/DAL/Context/MessageContext.cs
+++ Services/Message/MultiShop.Message/DAL/Context/MessageContext.cs
@@ -1,7 +1,14 @@
-﻿namespace MultiShop.Message.DAL.Entities
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MultiShop.Message.DAL.Entities
 {
-    public class MessageContext
+    public class MessageContext : DbContext
     {
-  
+        public MessageContext(DbContextOptions<MessageContext> options):base(options) 
+        {  
+        }
+
+        public DbSet<UserMessage> UserMessages { get; set; }
     }
 }
--- Services/Message/MultiShop.Message/Program.cs
+++ Services/Message/MultiShop.Message/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Message.DAL.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<MessageContext>(opt=>
+{
+    opt.UseNpgsql(confi)
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
