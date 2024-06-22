7559eeeb6d9417fddaeed9fc6db9cd146f6bca90
aleynaflo <selekaleyna@gmail.com>
2024-06-22 06:19:04 +0300

    382-Message ve Auto Mapper

--- Services/Message/MultiShop.Message/Services/UserMessageService.cs
+++ Services/Message/MultiShop.Message/Services/UserMessageService.cs
@@ -1,6 +1,73 @@
-﻿namespace MultiShop.Message.Services
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Message.DAL.Entities;
+using MultiShop.Message.Dtos;
+
+namespace MultiShop.Message.Services
 {
     public class UserMessageService : IUserMessageService
     {
+        private readonly MessageContext _messageContext;
+        private readonly IMapper _mapper;
+
+
+        public UserMessageService(MessageContext messageContext, IMapper mapper)
+        {
+            _messageContext = messageContext;
+            _mapper = mapper;
+        }
+
+        public async Task CreateMessageAsync(CreateMessageDto createMessageDto)
+        { 
+            var value = _mapper.Map<UserMessage>(createMessageDto);
+            await _messageContext.UserMessages.AddAsync(value);
+        }
+
+        public async Task DeleteMessageAsync(int id)
+        {
+            var values = await _messageContext.UserMessages.FindAsync(id);
+            _messageContext.UserMessages.Remove(values);
+        }
+
+        public async Task<List<ResultMessageDto>> GetAllMessageAsync()
+        {
+            var values = await _messageContext.UserMessages.ToListAsync();
+            return _mapper.Map<List<ResultMessageDto>>(values);
+        }
+
+        public async Task<GetByIdMessageDto> GetByIdMessageAsync(int id)
+        {
+            var value = await _messageContext.UserMessages.FindAsync(id);
+            return _mapper.Map<GetByIdMessageDto>(value);
+        }
+         
+
+        public async Task<List<ResultInboxMessageDto>> GetInboxMessageAsync(string id)
+        {
+            var values = await _messageContext.UserMessages.Where(x=>x.ReceiverId == id).ToListAsync();
+            return _mapper.Map<List<ResultInboxMessageDto>>(values);
+        }
+
+        public async Task<List<ResultSendboxMessageDto>> GetSendboxMessageAsync(string id)
+        {
+            var values = await _messageContext.UserMessages.Where(x => x.SenderId == id).ToListAsync();
+            return _mapper.Map<List<ResultSendboxMessageDto>>(values);
+        } 
+
+        public async Task UpdateMessageAsync(UpdateMessageDto updateMessageDto)
+        {
+            var value = _mapper.Map<UserMessage>(updateMessageDto);
+            _messageContext.UserMessages.Update(value);
+        }
+
+        //public int GetMessageCountRate(string code)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<ResultMessageDto> GetCodeDetailByCodeAsync(string code)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
