be256bf9af91346f5618cb6348a3ba0b6552229d
aleynaflo <selekaleyna@gmail.com>
2024-07-08 05:15:17 +0300

    400-Müşterilerin Adres Listesinde UI Kısmı

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Migrations/20240708015207_mig4.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Cargo.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UsetCustomerId",
+                table: "CargoCustomers",
+                newName: "UserCustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserCustomerId",
+                table: "CargoCustomers",
+                newName: "UserCustomerId");
+        }
+    }
+}
