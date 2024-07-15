36f3102ddfe3c668eb1eb8dfd096e2a9adaa8a55
aleynaflo <selekaleyna@gmail.com>
2024-07-15 18:47:51 +0300

    440-Controller Kısmı

--- Services/MultiShop.Images.WebUI/MultiShop.Images.WebUI/Program.cs
+++ Services/MultiShop.Images.WebUI/MultiShop.Images.WebUI/Program.cs
@@ -1,6 +1,11 @@
+using MultiShop.Images.WebUI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<GCSConfigOptions>(builder.Configuration);
+builder.Services.AddSingleton<ICloudStorageService, CloudStorageService>(); 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
